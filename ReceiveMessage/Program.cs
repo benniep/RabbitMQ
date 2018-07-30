5c47d71fad03a2de1266bff8d342f137c9b93de4
Bennie Potgieter <benniep@absamail.co.za>
2018-07-30 21:28:34 +0200

    RabbitMQ send and receive apps

---
+++ ReceiveMessage/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveMessage
+{
+    class Program
+    {
+        private const string QueueName = "StandardQueue_ProgramTest";
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        static void Main(string[] args)
+        {
+            CreateQueue();
+            var consumer = new EventingBasicConsumer(_model);
+            // we first want to check if the message that we take from the queue is our message we want
+            const bool autoAck = false;
+            _model.BasicConsume(QueueName, autoAck, consumer);
+            // This event will receive the message from the queue.
+            consumer.Received += (model, ea) =>
+            {
+                var msgBody = ea.Body;
+                string message = System.Text.Encoding.Unicode.GetString(msgBody);
+                if(message.ToLower().Contains("my name is,"))
+                { // Test is this is our message with the name to display it
+                    if(ValidateMessage(message))
+                    {
+                        var split = message.Split(',');
+                        System.Console.WriteLine("Hello {0}, I am your father!", split[1]);
+                        _model.BasicAck(ea.DeliveryTag, false);
+                    }
+                }
+            };
+            // Stay in listen mode until the user press enter.
+            Console.WriteLine("[*] Waiting for messages." +
+                                "To exit press ENTER");
+            Console.ReadLine();
+            //Close connection to RabbitMQ and exit
+            _model.Close();
+            _connection.Close();
+            
+        }
+        /// <summary>Validate if this is  valid message</summary>
+        /// <param name="msg">Message Text</param>
+        private static bool ValidateMessage(string msg)
+        {
+            var result = false;
+            var split = msg.Split(',');
+            if(!split[1].Trim().Equals(string.Empty))
+            {
+                return true;
+            }
+            return result;
+        }
+        /// <summer>Connect and create a queue to RabbitMQ</summer>
+        private static void CreateQueue()
+        {
+            _factory = new ConnectionFactory {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                RequestedHeartbeat = 30 
+            };
+            // Open connection to RabbitMQ
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+    }
+}
---
+++ SendMessage/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+
+namespace SendMessage
+{
+    class Program
+    {
+        private const string QueueName = "StandardQueue_ProgramTest";
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        static void Main(string[] args)
+        {
+            DisplayInputMessage();
+            var name = string.Empty;
+            // Do loop until the user wants to end the send message application.
+            do  
+            {
+                name = Console.ReadLine();
+                if (!name.Trim().Equals(string.Empty) && !name.ToUpper().Equals("EXIT"))
+                {
+                    CreateQueue();
+                    SendMessageToMQ(name);
+                    name = string.Empty;
+                    DisplayInputMessage();
+                }
+                else if(name.ToUpper().Trim().Equals("EXIT"))
+                {
+                    // Close all connections and exit the program
+                    if(_model != null)
+                    {
+                        _model.Close();
+                        _connection.Close();
+                    }
+                    return;
+                }
+                else
+                {
+                    DisplayInputMessage();
+                }
+
+            } while (true);
+        }
+        /// <summer>Connect and create a queue to RabbitMQ</summer>
+        private static void CreateQueue()
+        {
+            _factory = new ConnectionFactory {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                RequestedHeartbeat = 30 
+            };
+            // Create connection to RabbitMQ
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+
+        /// <summary>Send a message to RabbitMQ</summary>
+        /// <param name="name">Name entered by the user</param>
+        private static void SendMessageToMQ(string name)
+        {
+            var message = "Hello my name is, " + name;
+            var messagebody =  System.Text.Encoding.Unicode.GetBytes(message);
+            _model.BasicPublish("", QueueName, null, messagebody);
+            Console.WriteLine("[x] Message Sent: {0}", message);
+        }
+        /// <summary>Display message to the user</sumary>
+        private static void DisplayInputMessage()
+        {
+            Console.WriteLine("Enter your name: (To exit the program, type 'exit'.)");
+        }
+    }
+}
